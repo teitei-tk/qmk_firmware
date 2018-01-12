572e3e2ad562563ad493adc71c9df70a31b306a3
Yoshikatsu Higa <teitei.tk@gmail.com>
2018-01-12 12:55:19 +0900

    modify keybind

--- keyboards/ergodox_ez/keymaps/teitei-tk/keymap.c
+++ keyboards/ergodox_ez/keymaps/teitei-tk/keymap.c
@@ -25,13 +25,13 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * | Tab    |   Q  |   W  |   E  |   R  |   T  |      |           |      |   Y  |   U  |   I  |   O  |   P  |   @    |
  * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
  * | Ctrl   |   A  |   S  |   D  |   F  |   G  |------|           |------|   H  |   J  |   K  |   L  |  [{  |   }]   |
- * |--------+------+------+------+------+------|  L1  |           |  L1  |------+------+------+------+------+--------|
+ * |--------+------+------+------+------+------|  L1  |           |  '"  |------+------+------+------+------+--------|
  * | LShift |   Z  |   X  |   C  |   V  |   B  |      |           |      |   N  |   M  |  ,<  |  .>  |  /?  | RShift |
  * `--------+------+------+------+------+-------------'           `-------------+------+------+------+------+--------'
- *   |      |      |  Opt |  Cmd | Eisu |                                       | kana |  ;+  |  :*  |      |    _   |
+ *   |      |      |  Opt |  Cmd | Eisu |                                       | kana |  ;:  |  :*  |  `   |    _   |
  *   `----------------------------------'                                       `------------------------------------'
  *                                     ,--------------.           ,--------------.
- *                                     |   :  |   +   |           |  =   |   :*  |
+ *                                     |   *  |   +   |           |  =   |   :  |
  *                             ,-------|------|-------|           |------+-------+-------.
  *                             |       |      | Left  |           |  Up  |       |       |
  *                             | Back  |  DEL |-------|           |------| Space | Enter |
@@ -46,24 +46,24 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_ESC,         KC_1,       KC_2,     KC_3,       KC_4,      KC_5,   KC_6,
         KC_TAB,         KC_Q,       KC_W,     KC_E,       KC_R,      KC_T,   KC_NO,
         KC_LCTL,        KC_A,       KC_S,     KC_D,       KC_F,      KC_G,
-        KC_LSFT,        KC_Z,       KC_X,     KC_C,       KC_V,      KC_B,   TG(SYMB),
+        KC_LSFT,        KC_Z,       KC_X,     KC_C,       KC_V,      KC_B,   KC_QUOTE,
         KC_NO,          KC_NO,      KC_LALT,  KC_LGUI,    KC_LANG2,
 
-                        KC_COLON,   KC_PLUS,
-
-                                    KC_LEFT,
-        KC_BSPACE,      KC_DELETE,  KC_RIGHT,
+                        KC_ASTERISK,   KC_PLUS,   /* KC_KP_EQUAL,    KC_COLON, */
+                                       KC_LEFT,   /* KC_UP, */
+        KC_BSPACE,      KC_DELETE,     KC_RIGHT,  /* KC_DOWN, KC_SPACE, KC_ENT */
 
         // right hand
-        KC_7,           KC_8,       KC_9,       KC_0,       KC_MINS,    KC_EQL,     KC_JYEN,
-        KC_NO,          KC_Y,       KC_U,       KC_I,       KC_O,       KC_P,       KC_LBRC,
+        KC_7,           KC_8,       KC_9,       KC_0,       KC_MINS,    KC_EQL,     KC_PIPE,
+        KC_NO,          KC_Y,       KC_U,       KC_I,       KC_O,       KC_P,       KC_AT,
                         KC_H,       KC_J,       KC_K,       KC_L,       KC_LBRC,    KC_RBRC,
         TG(SYMB),       KC_N,       KC_M,       KC_COMM,    KC_DOT,     KC_SLSH,    KC_RSFT,
-                                    KC_LANG1,   KC_SCLN,    KC_QUOT,    KC_NO,      KC_RO,
+                                    KC_LANG1,   KC_SCLN,    KC_BSLASH,  KC_GRAVE,   KC_UNDERSCORE,
 
-        KC_KP_EQUAL,    KC_QUOT,
-        KC_UP,
-        KC_DOWN,        KC_BSPC, KC_ENT
+
+        /*              KC_ASTERISK,   KC_PLUS, */  KC_KP_EQUAL,    KC_COLON,
+        /*                             KC_LEFT, */  KC_UP,
+        /* KC_BSPACE,   KC_DELETE,     KC_RIGHT,*/  KC_DOWN, KC_SPACE, KC_ENT
     ),
 /* Keymap 1: Symbol Layer
  *
